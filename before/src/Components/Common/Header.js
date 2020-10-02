9f249489bd2070117af8462cda70db407c8c6035
Tanisha Agarwal <taagarw@microsoft.com>
2020-10-02 15:15:35 +0530

    created an initial structure for our React project

---
+++ before/src/Components/Common/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {NavLink} from "react-router-dom";
+
+function Header (){
+    const activeStyle = {color: "#F15B2A"}
+    return(<nav>
+        <NavLink to="/" activeStyle={activeStyle} exact>Home</NavLink>{" | "}
+        <NavLink to="/about" activeStyle={activeStyle}>About</NavLink> {" | "}
+        <NavLink to="/courses" activeStyle={activeStyle}>Courses</NavLink>
+
+
+    </nav>
+    );
+}
+
+export default Header;
